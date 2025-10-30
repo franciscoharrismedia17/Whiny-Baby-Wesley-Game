97d89628260a7ff4ac8ab5a65bc06bd4def537cc
franciscoharrismedia17 <francisco@harrismediallc.com>
2025-10-30 09:40:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -247,8 +247,8 @@ const OIL_NO_OVERLAY_CONFIG = {
   offset: { x: 0, y: 0 },
   scale: 1.8
 };
-const MAGA_OVERLAY_POSITION = { x: WIDTH / -2, y: 450 };
-const OIL_OVERLAY_POSITION = { x: WIDTH / -2, y: 450 };
+const MAGA_OVERLAY_POSITION = { x: WIDTH / -30, y: 450 };
+const OIL_OVERLAY_POSITION = { x: WIDTH / -10, y: 450 };
 let hillaryVictoryPending = false;
 let hillaryVictoryTimeout = null;
 let playInputLocked = false;
